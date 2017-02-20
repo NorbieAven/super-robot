4b847032d1d7a56b8360348606ba517d0cebb941
Norbie Aven <norbie.aven@pragmanila.com>
2017-02-20 18:54:00 +0800

    new themes and modified files

--- themes/sydney/single-career.php
+++ themes/sydney/single-career.php
@@ -6,7 +6,18 @@
  */
 
 get_header(); ?>
+<style>
+.panel, .panel-group .panel-heading+.panel-collapse>.panel-body{
+    border: none;
+}
+.panel-default .panel-heading {
+      /*border: 1px solid silver; --------- remove this line ----------*/
+      min-height: 100px;
+      background-color: white;
+      border-radius: 0px;
 
+    }
+</style>
 	<?php if (get_theme_mod('fullwidth_single')) { //Check if the post needs to be full width
 		$fullwidth = 'fullwidth';
 	} else {
@@ -24,16 +35,27 @@ get_header(); ?>
 
 			<?php sydney_post_navigation(); ?>
 
-		<a href="<?php the_permalink(); ?>"><button type="button" class="btn btn-success">Apply Now!</button></a>
+		<div class="section">
+					<div class="container">
+						<!-- Collapse -->
+							<div class="panel-group" id="accordion">
+									<div class="panel-heading">
+											<button style="margin-bottom: 20px;" type="button" class="btn btn-success" data-toggle="collapse" data-target="#collapse-1" data-parent="#Accordion">Apply Now
+											</button>
+									</div>
+									<div id="collapse-1" class="collapse">
+										<div class="panel-body">
+											 <?php echo do_shortcode( '[contact-form-7 id="203" title="Application Form"] '); ?>
+										</div>
+									</div>
+								</div>
+							</div>
+						<!--end Collapse-->
+					</div>
+				</div>
 
 		<?php endwhile; // end of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
-	<?php do_action('sydney_after_content'); ?>
-
-<?php if ( get_theme_mod('fullwidth_single', 0) != 1 ) {
-	get_sidebar();
-} ?>
 <?php get_footer(); ?>
